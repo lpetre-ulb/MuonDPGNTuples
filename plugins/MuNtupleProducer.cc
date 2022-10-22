cc3fe10bde13d4a171fc414e96103052fbac073a
Francesco Ivone <francesco.ivone@cern.ch>
2022-10-22 18:34:03 +0200

    Code formatting

--- plugins/MuNtupleProducer.cc
+++ plugins/MuNtupleProducer.cc
@@ -1,5 +1,5 @@
 /** \class MuNtupleProducer MuNtupleProducer.cc MuDPGAnalysis/MuonDPGNtuples/src/MuNtupleProducer.cc
- *  
+ *
  * Steering class: the edm::EDAnalyzer for MuDPGNtuple prdouction
  *
  * \author C. Battilana (INFN BO)
@@ -13,7 +13,7 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
 
-#include "FWCore/Framework/interface/Event.h" 
+#include "FWCore/Framework/interface/Event.h"
 #include "DataFormats/Common/interface/Handle.h"
 
 #include "FWCore/Framework/interface/ConsumesCollector.h"
@@ -41,77 +41,72 @@
 
 #include <iostream>
 
-MuNtupleProducer::MuNtupleProducer( const edm::ParameterSet & config )
+MuNtupleProducer::MuNtupleProducer(const edm::ParameterSet &config)
 {
 
   usesResource("TFileService");
   edm::Service<TFileService> fileService;
   bool isMC = static_cast<bool>(config.getParameter<bool>("isMC"));
   bool SaveVFATStatus = static_cast<bool>(config.getParameter<bool>("SaveVFATStatus"));
-  m_tree = std::shared_ptr<TTree>(fileService->make<TTree>("MuDPGTree","Mu DPG Tree"));
-  
-  
-  m_config = std::make_shared<MuNtupleConfig>(MuNtupleConfig(config,consumesCollector()));
-  
+  m_tree = std::shared_ptr<TTree>(fileService->make<TTree>("MuDPGTree", "Mu DPG Tree"));
+
+  m_config = std::make_shared<MuNtupleConfig>(MuNtupleConfig(config, consumesCollector()));
+
   m_fillers.push_back(std::make_unique<MuNtupleEventFiller>(consumesCollector(), m_config, m_tree, "event"));
 
-  //m_fillers.push_back(std::make_unique<MuNtupleDTDigiFiller>(consumesCollector(), m_config, m_tree, "dtDigi",    MuNtupleDTDigiFiller::Tag::PH1));
-  //m_fillers.push_back(std::make_unique<MuNtupleDTDigiFiller>(consumesCollector(), m_config, m_tree, "ph2DtDigi", MuNtupleDTDigiFiller::Tag::PH2));
+  // m_fillers.push_back(std::make_unique<MuNtupleDTDigiFiller>(consumesCollector(), m_config, m_tree, "dtDigi",    MuNtupleDTDigiFiller::Tag::PH1));
+  // m_fillers.push_back(std::make_unique<MuNtupleDTDigiFiller>(consumesCollector(), m_config, m_tree, "ph2DtDigi", MuNtupleDTDigiFiller::Tag::PH2));
 
-  //m_fillers.push_back(std::make_unique<MuNtupleDTSegmentFiller>(consumesCollector(), m_config, m_tree, "dtSeg",    MuNtupleDTSegmentFiller::Tag::PH1));
-  //m_fillers.push_back(std::make_unique<MuNtupleDTSegmentFiller>(consumesCollector(), m_config, m_tree, "ph2DtSeg", MuNtupleDTSegmentFiller::Tag::PH2));
-  
+  // m_fillers.push_back(std::make_unique<MuNtupleDTSegmentFiller>(consumesCollector(), m_config, m_tree, "dtSeg",    MuNtupleDTSegmentFiller::Tag::PH1));
+  // m_fillers.push_back(std::make_unique<MuNtupleDTSegmentFiller>(consumesCollector(), m_config, m_tree, "ph2DtSeg", MuNtupleDTSegmentFiller::Tag::PH2));
+
   // m_fillers.push_back(std::make_unique<MuNtupleGEMDigiFiller>(consumesCollector(), m_config, m_tree, "gemDigi"));
-  if (SaveVFATStatus) m_fillers.push_back(std::make_unique<MuNtupleGEMVFATStatusFiller>(consumesCollector(),m_config,m_tree,"gemOHStatus"));
-  
+  if (SaveVFATStatus)
+    m_fillers.push_back(std::make_unique<MuNtupleGEMVFATStatusFiller>(consumesCollector(), m_config, m_tree, "gemOHStatus"));
+
   m_fillers.push_back(std::make_unique<MuNtupleGEMRecHitFiller>(consumesCollector(), m_config, m_tree, "gemRecHit"));
 
   m_fillers.push_back(std::make_unique<MuNtupleGEMSegmentFiller>(consumesCollector(), m_config, m_tree, "gemSegment"));
 
-  m_fillers.push_back(std::make_unique<MuNtupleGEMMuonFiller>(consumesCollector(),m_config,m_tree,"mu"));
-  
-  if (isMC) m_fillers.push_back(std::make_unique<MuNtupleGEMSimHitFiller>(consumesCollector(), m_config, m_tree, "gemSimHit"));
-  
+  m_fillers.push_back(std::make_unique<MuNtupleGEMMuonFiller>(consumesCollector(), m_config, m_tree, "mu"));
+
+  if (isMC)
+    m_fillers.push_back(std::make_unique<MuNtupleGEMSimHitFiller>(consumesCollector(), m_config, m_tree, "gemSimHit"));
 }
 
-
-void MuNtupleProducer::beginJob() 
+void MuNtupleProducer::beginJob()
 {
 
-  for (const auto & filler : m_fillers) 
-    {
-      filler->initialize();
-      filler->clear();
-    }
-  
+  for (const auto &filler : m_fillers)
+  {
+    filler->initialize();
+    filler->clear();
+  }
 }
 
-void MuNtupleProducer::beginRun(const edm::Run & run, const edm::EventSetup & environment )
+void MuNtupleProducer::beginRun(const edm::Run &run, const edm::EventSetup &environment)
 {
 
   m_config->getES(run, environment);
-  
 }
 
-void MuNtupleProducer::endJob() 
+void MuNtupleProducer::endJob()
 {
 
   m_tree->GetCurrentFile()->Write();
-
 }
 
-void MuNtupleProducer::analyze(const edm::Event & ev, const edm::EventSetup & environment )
+void MuNtupleProducer::analyze(const edm::Event &ev, const edm::EventSetup &environment)
 {
 
   m_config->getES(environment);
 
-  for (const auto & filler : m_fillers) 
-    {
-      filler->fill(ev);
-    }
- 
- m_tree->Fill();
- 
+  for (const auto &filler : m_fillers)
+  {
+    filler->fill(ev);
+  }
+
+  m_tree->Fill();
 }
 
 // define this as a plug-in
--- src/MuNtupleGEMVFATStatusFiller.cc
+++ src/MuNtupleGEMVFATStatusFiller.cc
@@ -4,11 +4,9 @@
 #include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 
-MuNtupleGEMVFATStatusFiller::MuNtupleGEMVFATStatusFiller(edm::ConsumesCollector && collector,
-					   const std::shared_ptr<MuNtupleConfig> config,
-					   std::shared_ptr<TTree> tree, const std::string & label) :
-
-  MuNtupleBaseFiller(config, tree, label)
+MuNtupleGEMVFATStatusFiller::MuNtupleGEMVFATStatusFiller(edm::ConsumesCollector &&collector,
+                                                         const std::shared_ptr<MuNtupleConfig> config,
+                                                         std::shared_ptr<TTree> tree, const std::string &label) : MuNtupleBaseFiller(config, tree, label)
 {
     // I couldn't manage to get the GEM OH Status collections with the usual conditionalGet. So I have copied DQM https://github.com/cms-sw/cmssw/blob/38405a5b319be8ec094c981d6b45320aa577676a/DQM/GEM/plugins/GEMDAQStatusSource.cc#L9
     m_gemOHStatusToken = collector.consumes<GEMOHStatusCollection>(edm::InputTag("muonGEMDigis", "OHStatus"));
@@ -21,19 +19,15 @@ MuNtupleGEMVFATStatusFiller::~MuNtupleGEMVFATStatusFiller()
 
 void MuNtupleGEMVFATStatusFiller::initialize()
 {
-
-  m_tree->Branch((m_label + "_station").c_str(), &m_OHStatus_station);
-  m_tree->Branch((m_label + "_region").c_str(), &m_OHStatus_region);
-  m_tree->Branch((m_label + "_chamber").c_str(), &m_OHStatus_chamber);
-  m_tree->Branch((m_label + "_layer").c_str(), &m_OHStatus_layer);
-  m_tree->Branch((m_label + "_VFATMasked").c_str(), &m_OHStatus_VFATs);
-
-
+    m_tree->Branch((m_label + "_station").c_str(), &m_OHStatus_station);
+    m_tree->Branch((m_label + "_region").c_str(), &m_OHStatus_region);
+    m_tree->Branch((m_label + "_chamber").c_str(), &m_OHStatus_chamber);
+    m_tree->Branch((m_label + "_layer").c_str(), &m_OHStatus_layer);
+    m_tree->Branch((m_label + "_VFATMasked").c_str(), &m_OHStatus_VFATs);
 }
 
 void MuNtupleGEMVFATStatusFiller::clear()
 {
-  
     m_OHStatus_region.clear();
     m_OHStatus_station.clear();
     m_OHStatus_chamber.clear();
@@ -41,69 +35,74 @@ void MuNtupleGEMVFATStatusFiller::clear()
     m_OHStatus_VFATs.clear();
 }
 
-void MuNtupleGEMVFATStatusFiller::fill(const edm::Event & ev)
+void MuNtupleGEMVFATStatusFiller::fill(const edm::Event &ev)
 {
 
-  clear();
+    clear();
 
-  const auto gem = m_config->m_gemGeometry;
-  edm::Handle<GEMOHStatusCollection> OH_StatusCollection;
-  ev.getByToken(m_gemOHStatusToken, OH_StatusCollection);
+    const auto gem = m_config->m_gemGeometry;
+    edm::Handle<GEMOHStatusCollection> OH_StatusCollection;
+    ev.getByToken(m_gemOHStatusToken, OH_StatusCollection);
 
-  if (OH_StatusCollection.isValid()){
-          for (auto ohIt = OH_StatusCollection->begin(); ohIt != OH_StatusCollection->end(); ohIt++){
-                  const GEMDetId& gem_id = (*ohIt).first;
-                  const GEMOHStatusCollection::Range &range = (*ohIt).second;
+    if (OH_StatusCollection.isValid())
+    {
+        for (auto ohIt = OH_StatusCollection->begin(); ohIt != OH_StatusCollection->end(); ohIt++)
+        {
+            const GEMDetId &gem_id = (*ohIt).first;
+            const GEMOHStatusCollection::Range &range = (*ohIt).second;
 
-                  int region = gem_id.region();
-                  int station = gem_id.station();
-                  int chamber = gem_id.chamber();
-                  int layer = gem_id.layer();
+            int region = gem_id.region();
+            int station = gem_id.station();
+            int chamber = gem_id.chamber();
+            int layer = gem_id.layer();
 
-                  for (auto OHStatus = range.first; OHStatus != range.second; ++OHStatus) {
+            for (auto OHStatus = range.first; OHStatus != range.second; ++OHStatus)
+            {
 
-                      const uint32_t vfatMask = OHStatus->vfatMask();
-                      // TO DO: Understand how these two vars work
-                      // const uint32_t zsMask = OHStatus->vfatMask();
-                      // const uint32_t missingVFATs = OHStatus->missingVFATs(;)
+                const uint32_t vfatMask = OHStatus->vfatMask();
+                // TO DO: Understand how these two vars work
+                // const uint32_t zsMask = OHStatus->vfatMask();
+                // const uint32_t missingVFATs = OHStatus->missingVFATs(;)
 
+                // skip if all VFATs are valid i.e. vfatMask = FFFFFF
+                if (vfatMask == 16777215)
+                {
+                    continue;
+                }
 
-                      // skip if all VFATs are valid i.e. vfatMask = FFFFFF
-                      if (vfatMask == 16777215) 
-                        {   
-                            continue;
+                else
+                {
+                    std::bitset<24> vfatMaskbits(vfatMask);
+                    // std::bitset<24> zsMaskbits(zsMask);
+                    // std::bitset<24> missingVFATbits(missingVFATs);
+                    // std::cout<<"re: "<<region<<"\tch: "<<chamber<<"\tly: "<<layer<<"\tVFATMask: "<<vfatMaskbits<<std::endl;
+                    // std::cout<<"re: "<<region<<"\tch: "<<chamber<<"\tly: "<<layer<<"\tVFATMask: "<<vfatMaskbits<<"\tzsMask: "<<zsMaskbits.to_string()<<"\tmissingVFAT: "<<missingVFATbits.to_string()<<std::endl;
+
+                    // TODO: Improve conversion and vector filling
+                    int VFAT_Pos = 23;
+                    for (auto i : vfatMaskbits.to_string())
+                    {
+                        if (i == '0')
+                        {
+                            m_OHStatus_station.push_back(station);
+                            m_OHStatus_region.push_back(region);
+                            m_OHStatus_chamber.push_back(chamber);
+                            m_OHStatus_layer.push_back(layer);
+                            // TODO: double check for VFAT position
+                            m_OHStatus_VFATs.push_back(VFAT_Pos);
                         }
-                        
-                      else{
-                          std::bitset<24> vfatMaskbits(vfatMask);
-                          // std::bitset<24> zsMaskbits(zsMask);
-                          // std::bitset<24> missingVFATbits(missingVFATs);
-                          //std::cout<<"re: "<<region<<"\tch: "<<chamber<<"\tly: "<<layer<<"\tVFATMask: "<<vfatMaskbits<<std::endl;
-                          //std::cout<<"re: "<<region<<"\tch: "<<chamber<<"\tly: "<<layer<<"\tVFATMask: "<<vfatMaskbits<<"\tzsMask: "<<zsMaskbits.to_string()<<"\tmissingVFAT: "<<missingVFATbits.to_string()<<std::endl;
 
-                          // TODO: Improve conversion and vector filling
-                          int VFAT_Pos = 23;
-                          for(auto i: vfatMaskbits.to_string()){
-                              if (i == '0'){
-                                  m_OHStatus_station.push_back(station);
-                                  m_OHStatus_region.push_back(region);
-                                  m_OHStatus_chamber.push_back(chamber);
-                                  m_OHStatus_layer.push_back(layer);
-                                  //TODO: double check for VFAT position
-                                  m_OHStatus_VFATs.push_back(VFAT_Pos);
-                              }
-                                  
-                              VFAT_Pos--;
-                          }// Loop over VFAT Postions
-                      }//At least 1 masked VFAT   
-                  }//Loop over the OHStatus
-          }//Loop through the collection
-  }//OH_StatusCollection is valid
+                        VFAT_Pos--;
+                    } // Loop over VFAT Postions
+                }     // At least 1 masked VFAT
+            }         // Loop over the OHStatus
+        }             // Loop through the collection
+    }                 // OH_StatusCollection is valid
 
-  else{
-      std::cout<<"OH_Statuscollection is invalid"<<std::endl;
-  }
-  
-  return;
+    else
+    {
+        std::cout << "OH_Statuscollection is invalid" << std::endl;
+    }
 
+    return;
 }
